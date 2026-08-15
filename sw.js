557f9f0203cd91086080b271dfea0252d5883af1
May9030 <may9030@163.com>
2026-08-15 17:49:55 +0800

    auto update 2026-08-15 17:49:55

---
+++ sw.js
@@ -0,0 +1,34 @@
+// 可转债工作台 Service Worker：网络优先，缓存兜底（断网时显示最后一次成功加载的看板）
+var CACHE = 'cb-dash-v1';
+var CORE = ['./', './index.html', './manifest.webmanifest', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(CACHE).then(function (c) {
+      return c.addAll(CORE).catch(function () {});
+    }).then(function () { return self.skipWaiting(); })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keys) {
+      return Promise.all(keys.filter(function (k) { return k !== CACHE; }).map(function (k) { return caches.delete(k); }));
+    }).then(function () { return self.clients.claim(); })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  if (e.request.method !== 'GET') { return; }
+  e.respondWith(
+    fetch(e.request).then(function (res) {
+      var copy = res.clone();
+      caches.open(CACHE).then(function (c) { c.put(e.request, copy); });
+      return res;
+    }).catch(function () {
+      return caches.match(e.request, { ignoreSearch: true }).then(function (hit) {
+        return hit || (e.request.mode === 'navigate' ? caches.match('./index.html') : undefined);
+      });
+    })
+  );
+});
